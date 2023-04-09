a7887932648887c863230134a98b2e0c20cb68b0
Tarun Manjunath <tarun.m1@idctechnologies.com>
2023-04-09 15:22:42 +0530

    Delete Loan-Product-Configuration directory

--- Loan-Product-Configuration/src/main/java/com/loanproduct/Loan/Product/Configuration/Repository/CustomerCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.loanproduct.Loan.Product.Configuration.Repository;
-
-import com.loanproduct.Loan.Product.Configuration.Entity.Customer_Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerCategoryRepository extends JpaRepository<Customer_Category, Integer> {
-}
